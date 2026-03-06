3ca2175ab2b1f794be608933e63d7893c460083a
Olivier Ribiere <olivier.ribiere@gmail.com>
2026-03-06 03:38:30 +0100

    feat: add initial internationalization setup with English and French translation files

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,22 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import enTranslation from "../locales/en/translation.json";
+import frTranslation from "../locales/fr/translation.json";
 
 const resources = {
   en: {
-    translation: {
-      "hello": "Hello World",
-      "wip.title": "Work in progress",
-      "wip.subtitle": "We're currently building something amazing.",
-      "wip.expectedLaunch": "Tomorrow, kind off",
-    }
+    translation: enTranslation
   },
   fr: {
-    translation: {
-      "hello": "Bonjour le monde",
-      "wip.title": "Travail en cours",
-      "wip.subtitle": "Nous construisons actuellement quelque chose d'incroyable.",
-      "wip.expectedLaunch": "Demain, ou presque",
-    }
+    translation: frTranslation
   }
 };
 
