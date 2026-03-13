ab553809dc6b854328afa6d1f1b68d22e335651b
Olivier Ribiere <olivier.ribiere@gmail.com>
2026-03-14 00:54:04 +0100

    Feat: Add content

--- src/features/about/components/AboutView.tsx
+++ src/features/about/components/AboutView.tsx
@@ -1,8 +1,6 @@
-import { useTranslation } from "react-i18next";
 import Timeline from "./Timeline";
 
 export function AboutView() {
-  const { t } = useTranslation();
 
   return (
     <div className="flex flex-col w-full max-w-4xl">
