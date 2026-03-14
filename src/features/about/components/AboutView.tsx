dd81cba43a305e4f5c643e923e010a77f50e7d17
Olivier Ribiere <olivier.ribiere@gmail.com>
2026-03-14 05:59:55 +0100

    fix: Update content

--- src/features/about/components/AboutView.tsx
+++ src/features/about/components/AboutView.tsx
@@ -1,10 +1,18 @@
+import { useTranslation } from "react-i18next";
 import Timeline from "./Timeline";
 
 export function AboutView() {
+  const { t } = useTranslation();
 
   return (
     <div className="flex flex-col w-full max-w-4xl">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+      <div className="flex flex-col gap-4 text-justify">
+        <span className="text-2xl font-bold">{t("about.title")}</span>
+        <p>{t("about.description1")}</p>
+        <p>{t("about.description2")}</p>
+        <p>{t("about.description3")}</p>
+        <p>{t("about.description4")}</p>
+      </div>
       <Timeline />
     </div>
   );
